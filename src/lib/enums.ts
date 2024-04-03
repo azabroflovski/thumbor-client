afa18b85d481a367f096da694c939a13d43d1154
azabroflovski <azabroflovski@gmail.com>
2024-04-03 10:43:01 +0500

    refactor(enums): add jsdoc comments

--- src/lib/enums.ts
+++ src/lib/enums.ts
@@ -1,17 +1,53 @@
+/**
+ * Enum representing different fitting types for an element.
+ */
 export enum FitInType {
+    /**
+     * Default fitting type.
+     */
     DEFAULT = 'DEFAULT',
+    /**
+     * Full fitting type.
+     */
     FULL = 'FULL',
+    /**
+     * Adaptive fitting type.
+     */
     ADAPTIVE = 'ADAPTIVE'
 }
 
+/**
+ * Enum representing vertical positions.
+ */
 export enum VerticalPosition {
+    /**
+     * Top vertical position.
+     */
     TOP = 'TOP',
+    /**
+     * Middle vertical position.
+     */
     MIDDLE = 'MIDDLE',
+    /**
+     * Bottom vertical position.
+     */
     BOTTOM = 'BOTTOM'
 }
 
+/**
+ * Enum representing horizontal positions.
+ */
 export enum HorizontalPosition {
+    /**
+     * Left horizontal position.
+     */
     LEFT = 'LEFT',
+    /**
+     * Center horizontal position.
+     */
     CENTER = 'CENTER',
+    /**
+     * Right horizontal position.
+     */
     RIGHT = 'RIGHT'
 }
